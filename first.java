e0c29da95d6e314b42c8d6237f79c5007671a4ca
sujaldey004 <sujaldey2920@gmail.com>
2023-12-23 01:12:30 +0530

    i am the only one

--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 public class first{
     public static void main(String[] args){
-        System.out.print("Hello World now i am going to change you");
+        System.out.print("Hello World now i am going to change you and you cant do anything.");
     }
 }
