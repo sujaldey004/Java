911ce7cb37113c7f1c6b1242351208f0147d9355
sujaldey004 <sujaldey2920@gmail.com>
2023-12-23 17:56:58 +0530

    Interface in java with multiple interface

--- ep14.java
+++ ep14.java
@@ -2,12 +2,32 @@ interface phone{
     void ringing();
     void calling();
 }
-class smartphone implements phone{
+interface camera{
+    void click();
+    void record();
+}
+interface musicplayer{
+    void playmusic();
+    void stopmusic();
+}
+class smartphone implements phone,camera,musicplayer{
     public void ringing(){
         System.out.println("ringing...");
     }
     public void calling(){
         System.out.println("calling...");
+    }
+    public void playmusic(){
+        System.out.println("music playing..");
+    }
+    public void stopmusic(){
+        System.out.println("music stop...");
+    }
+    public void click(){
+        System.out.println("capturing picture...");
+    }
+    public void record(){
+        System.out.println("recording video...");
     }
 }
 public class ep14 {
@@ -15,5 +35,9 @@ public class ep14 {
         smartphone sp = new smartphone();
         sp.calling();
         sp.ringing();
+        sp.record();
+        sp.click();
+        sp.playmusic();
+        sp.stopmusic();
     }
 }
