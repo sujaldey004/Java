6474de41b4040cc62c45c8e27c5448759cafc73c
sujaldey004 <sujaldey2920@gmail.com>
2023-12-23 20:03:15 +0530

    Default method in interface

---
+++ UseOfDefault.java
@@ -0,0 +1,56 @@
+interface myCamera{
+    void portraitmode();
+    void panormamode();
+
+
+    // This is a default mathod and its body in tnterface
+    default void record4k(){
+        System.out.println("Recording in 4k..");
+    }
+
+}
+
+
+interface myNetwork{
+    String[] availablewifi();
+    void getwifi(String network);
+}
+
+// we can also use methods of this class also
+class cellPhone{
+    void callNumber(){
+        System.out.println("caling...");
+    }
+    void pickCall(){
+        System.out.println("say helloooo when pick...");
+    }
+}
+
+
+class SamrtPhone extends cellPhone implements myCamera,myNetwork{
+    public void portraitmode(){
+        System.out.println("Portrait mode on...");
+    }
+    public void panormamode(){
+        System.out.println("Panorma mode on...");
+    }
+    public String[] availablewifi(){
+        System.out.println("Available wifi --> ");
+         String[] networklist = {"A", "B", "c"};
+         return networklist;
+    }
+    public void getwifi(String network){
+        System.out.println("Connecting to "+network);
+    }
+}
+public class UseOfDefault {
+    public static void main(String[] args) {
+        SamrtPhone sp = new SamrtPhone();
+        sp.record4k();
+        String[] a = sp.availablewifi();
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+        sp.getwifi("A");
+    }
+}
