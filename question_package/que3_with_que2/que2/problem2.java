4d3e4c7e9b46a91f3703bd4d1b4f9c6ec69a5279
sujaldey004 <sujaldey2920@gmail.com>
2023-12-27 21:14:52 +0530

    Problem2 on Package topic

---
+++ question_package/que3_with_que2/que2/problem2.java
@@ -0,0 +1,10 @@
+package question_package.que3_with_que2.que2;
+import java.util.Scanner;
+public class problem2 {
+    public static void main(String[] args) {
+        System.out.println("its is very simple, just enter your name = ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hii "+name+" welcome to java classes");
+    }
+}
