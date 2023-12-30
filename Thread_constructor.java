80f5506c9f00c7401d663e9aa4006e2a55471508
sujaldey004 <sujaldey2920@gmail.com>
2023-12-31 02:03:46 +0530

    Thread Constructor with both class and interface

---
+++ Thread_constructor.java
@@ -0,0 +1,36 @@
+/*class MyThread extends Thread{
+    MyThread(String name){
+        super(name);
+    }
+    public void run(){
+        System.out.println("Its running");
+    }
+}
+
+public class Thread_constructor {
+    public static void main(String[] args) {
+        MyThread mt = new MyThread("sam");
+        mt.start();
+        System.out.println("Thread id = "+mt.getId());
+        System.out.println("Thread name = "+mt.getName());
+    }
+}
+*/
+
+
+
+class TH implements Runnable{
+    public void run(){
+        System.out.println("Its running");
+    }
+}
+public class Thread_constructor{
+    public static void main(String[] args) {
+        TH t = new TH();
+        Thread tt = new Thread(t,"sam");
+
+        tt.start();
+        System.out.println("Thread id "+tt.getId());
+        System.out.println("Thread name "+tt.getName());
+    }
+}
