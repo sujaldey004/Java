6ef4f4972ffd26fbf7477c333e1305b63c0f2528
sujaldey004 <sujaldey2920@gmail.com>
2024-01-04 17:32:36 +0530

    Try catch baiscs

---
+++ TryCatch.java
@@ -0,0 +1,13 @@
+public class TryCatch {
+    public static void main(String[] args) {
+        double a = 5;
+        double b = 4;
+        try{
+            double c = a/b;
+            System.out.println("ans = "+c);
+        }
+        catch(Exception e){
+            System.out.println("abe pagal h kya");
+        }
+    }
+}
