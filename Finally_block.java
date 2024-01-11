ac9bc9eb0c44efdba9ea33f8d7437d12b1fed6a2
sujaldey004 <sujaldey2920@gmail.com>
2024-01-11 18:27:29 +0530

    without finally block it return from try block

---
+++ Finally_block.java
@@ -0,0 +1,21 @@
+public class Finally_block {
+    public static int demo(){
+        int a = 50;
+        int b = 2;
+        try{
+            int c = a/b;
+            return c;
+        }
+        catch(Exception e){
+            System.out.println("Cannot divide by zero");
+        }
+        //finally{           //  Finally keyword cannot be used without try-catch
+            System.out.println("Execute this also");
+       // }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int k = demo();
+        System.out.println(k);
+    }
+}
