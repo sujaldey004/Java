68cc7f8d5d62e51f3814e60d74a7c72654400a6f
sujaldey004 <sujaldey2920@gmail.com>
2024-01-11 19:18:30 +0530

    First try and understand the code then explain

---
+++ Restart_question/que2.java
@@ -0,0 +1,24 @@
+package Restart_question;
+
+import java.util.Scanner;
+
+public class que2 {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3,};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a index number = ");
+        int index;
+        int i = 0;
+        while(i < 5){
+            index = sc.nextInt();
+            try{
+                System.out.println("The value at index is = "+arr[index]);
+                break;
+            }
+            catch(Exception e){
+                System.out.println("Error");
+                i++;
+            }
+        }
+    }
+}
