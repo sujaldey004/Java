a28baab283b5454f4118a71ef6a4bcd052307f76
sujaldey004 <sujaldey2920@gmail.com>
2024-02-08 17:58:32 +0530

    Collection Framework -> Date and Time

---
+++ ADVANCE_JAVA/DateAndTime.java
@@ -0,0 +1,7 @@
+package ADVANCE_JAVA;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis()/1000/60/60/24/365);   // Java count milisecond from jan 1, 1970 and year from 1900
+    }
+}
