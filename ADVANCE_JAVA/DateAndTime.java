fbbadc36ae277e1fd4bc45998ae8bf578afd37f5
sujaldey004 <sujaldey2920@gmail.com>
2024-02-08 18:23:44 +0530

    Collection Framework -> Date and Time

--- ADVANCE_JAVA/DateAndTime.java
+++ ADVANCE_JAVA/DateAndTime.java
@@ -1,11 +1,14 @@
 package ADVANCE_JAVA;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class DateAndTime {
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis()/1000/60/60/24/365);   // Java count milisecond from jan 1, 1970 and year from 1900
-        Date d = new Date();
+        LocalDateTime d = LocalDateTime.now();  // This is the new methods of new API
+        Date d1 = new Date();
+        System.out.println(d1);
         System.out.println(d);
     }
 }
