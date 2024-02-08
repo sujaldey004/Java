6f14236b1917d96f1cc6c5f5b4cfa004e14727ef
sujaldey004 <sujaldey2920@gmail.com>
2024-02-08 18:54:13 +0530

    Collection Framework -> Date and Time methods by books

--- ADVANCE_JAVA/DateAndTime.java
+++ ADVANCE_JAVA/DateAndTime.java
@@ -5,10 +5,14 @@ import java.util.Date;
 
 public class DateAndTime {
     public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis()/1000/60/60/24/365);   // Java count milisecond from jan 1, 1970 and year from 1900
-        LocalDateTime d = LocalDateTime.now();  // This is the new methods of new API
+        //System.out.println(System.currentTimeMillis()/1000/60/60/24/365);   // Java count milisecond from jan 1, 1970 and year from 1900
+        //LocalDateTime d = LocalDateTime.now();  // This is the new methods of new API
         Date d1 = new Date();
-        System.out.println(d1);
-        System.out.println(d);
-    }
+        //System.out.println(d1);
+        //System.out.println(d);
+
+        System.out.println(String.format("%tc",d1));   // ⬇ These methods are from Reference book
+        System.out.println(String.format("%tr",d1));
+        System.out.println(String.format("%tA, %tB %td",d1,d1,d1));
+        }
 }
